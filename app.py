44dbbf0445c1ed47ed2677b09ff3a08af7fa82a4
MichaelOdons <michaelodonghanro287@gmail.com>
2026-03-04 22:53:18 +0100

    Update app.py to use gunicorn for Railway

--- app.py
+++ app.py
@@ -66,5 +66,4 @@ def predict():
         return f"Error in predict route: {e}"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
